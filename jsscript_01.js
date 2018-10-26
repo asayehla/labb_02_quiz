564ebc0d420306b8a028ac0482c97ee6dc40feda
Åsa Palholmen <asa@palholmen.se>
2018-10-26 11:59:34 +0200

    Redoing

--- jsscript_01.js
+++ jsscript_01.js
@@ -1,6 +1,5 @@
 var res;
 var resInObject;
-var answers = [];
 
 function shuffle(answerIndex) { //shuffle-function
   var random, x, i;
@@ -12,71 +11,64 @@ function shuffle(answerIndex) { //shuffle-function
   }
 }
 
-function makeQuiz() {
-  //Header
-  var headName = `<h2>${res.category}</h2>`;
+function makeQuiz(res) {
+  var headName = `<h2>${res[0].category}</h2>`; //Header
   document.getElementById("quizName").innerHTML = headName;
-  //end of Header
+  begin(res);
 
-  resInObject.results.forEach(function(res, index) {
+  function begin(res) {
+    question = [];
+    answeredQuestions = [];
+
+    for (var i = 0; i < 10; i++) {
+      var correctAnswer = resInObject.results[i].correct_answer;
+    //  console.log(correctAnswer);
+      var incorrectAnswers = resInObject.results[i].incorrect_answers;
+      //console.log(incorrectAnswer);
+      var answerIndex = []; //collecting every answer in a an array. nr 0 is correct answer.
+      answerIndex.push(0);
+      var answers = [];
+      answers.push(correctAnswer);
+      for (let i = 0; i < 3; i++) {
+        answerIndex.push(i + 1);
+        answers.push(incorrectAnswers[i]);
+      }
+      console.log(answers);
+
+
+
+    }   //viktig
+//console.log(answers); //ger mig bara den sista
+
     let questiondiv = document.createElement("div");
     questiondiv.classList.add("question");
-    questiondiv.id = index;
-    content.appendChild(questiondiv);
+    //questiondiv.id = index;
+    questionIndex.appendChild(questiondiv);
 
-    //collecting every answer in a an array. nr 0 is correct answer.
-    var answerIndex = [];
-    answerIndex.push(0);
-    answers.push(res.correct_answer);
-    for (let i = 0; i < 3; i++) {
-      answerIndex.push(i + 1);
-      answers.push(res.incorrect_answers[i]);
-    }
-    //console.log(answers); an array with all answers.
-    //console.log(answerIndex); //0123  0 är rätt svar.
-    shuffle(answerIndex);
-
+    //shuffle(answerIndex);
     var quest = `<p>${res.question}</p>`
-
-    /*var options = `<input type="radio" id="0">${answers[0]}
-           <input type="radio" id="1">${answers[1]}
-           <input type="radio" id="2">${answers[2]}
-           <input type="radio" id="3">${answers[3]}`;*/
-
-    for (i = 0; i < 4; i++) {
-      var htmlradio = document.createElement("INPUT");
-      htmlradio.setAttribute("type", "radio");
-      //var writeAnswer = `${answers[i]}`;
-      var htmlanswer = document.getElementById("answers");
-      content.appendChild(htmlradio);
-      //content.appendChild(writeAnswer);
-    }
-
-    var questionAnswers = quest;
-    document.getElementById(questiondiv.id).innerHTML = questionAnswers;
-
-  });
+    var options = `<input type="radio" id="0">${answers[0]}  <input type="radio" id="1">${answers[1]}    <input type="radio" id="2">${answers[2]}          <input type="radio" id="3">${answers[3]}`;
+    var questionAnswers = quest + options;
+    //document.getElementById(questiondiv.id).innerHTML = questionAnswers;
+  }
 }
 
+///////////////////////////
 function startQuiz() {
   var req = new XMLHttpRequest;
   req.onreadystatechange = function() {
     if (req.readyState == 4) {
       if (req.status == 200) {
-
-        //onloading the results from json, becouse the information is in an object.
-        resInObject = req.response;
-        res = resInObject.results[0];
-        makeQuiz();
+        resInObject = req.response; //onloading the results from json, becouse the information is in an object.
+        res = resInObject.results;
+        makeQuiz(res);
       }
     }
   };
-
   req.open("GET", "https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple");
   req.responseType = 'json';
   req.send();
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   startQuiz();
 });
